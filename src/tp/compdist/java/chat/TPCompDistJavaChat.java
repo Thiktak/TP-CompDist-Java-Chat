94e594d745d7713a6c5c8f1d98651b2ae4d7067b
Olivarès Georges <olivares.georges@gmail.com>
2013-05-03 14:09:09 +0200

    fist commit

---
+++ src/tp/compdist/java/chat/TPCompDistJavaChat.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+package tp.compdist.java.chat;
+
+/**
+ *
+ * @author Georges OLIVARES <dev@olivares-georges.fr>
+ */
+public class TPCompDistJavaChat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
